fdfc587af15bc369662cbfe0261bee0a18f08358
U-Zyn Chua <chua@uzyn.com>
2016-01-27 01:12:52 +0800

    Search for timestamp collision first before writing.

--- dynamodb.js
+++ dynamodb.js
@@ -16,32 +16,68 @@ module.exports = {
       if (err) {
         return context.fail(err);
       }
+      var timestamp = Math.floor(new Date(headers.lastmod).getTime() / 1000);
 
-      var params = {
-        TableName: process.env.AWS_DYNAMODB_TABLE,
-        Item: {
-          timestamp: Math.floor(new Date(headers.lastmod).getTime() / 1000),
-          locations: results
+      async.series({
+        validate: function (callback) {
+          var params = {
+            TableName: process.env.AWS_DYNAMODB_TABLE,
+            Select: 'COUNT',
+            KeyConditionExpression: "#ts = :tttt",
+            ExpressionAttributeNames: {
+              '#ts': 'timestamp'
+            },
+            ExpressionAttributeValues: {
+              ":tttt": timestamp
+            }
+          };
+
+          docClient.query(params, function (err, data) {
+            if (!err) {
+              if (data.Count === 0) {
+                return callback(null);
+              } else {
+                return callback(data);
+              }
+            }
+            return callback(err);
+          });
+
+        },
+
+        put: function (callback) {
+          var params = {
+            TableName: process.env.AWS_DYNAMODB_TABLE,
+            Item: {
+              timestamp: timestamp,
+              locations: results
+            }
+          };
+
+          var saved = false;
+          async.whilst( function() {
+              return !saved;
+            }, function (callback) {
+            docClient.put(params, function (err, data) {
+              saved = true;
+              if (err && err.retryable === true) {
+                saved = false;
+              }
+              callback(err, data);
+            });
+          }, function (err, data) {
+            return callback(err, results, headers);
+          });
         }
-      };
-
-      var saved = false;
-      async.whilst( function() {
-          return !saved;
-        }, function (callback) {
-        docClient.put(params, function (err, data) {
-          saved = true;
-          if (err && err.retryable === true) {
-            saved = false;
-          }
-          callback(err, data);
-        });
-      }, function (err, data) {
+      }, function (err, results, headers) {
         if (err) {
-          console.error(err);
+          console.log(err);
+          return context.fail(err);
+        } else {
+          return context.done(err, results.length + ' locations saved successfully with timestamp ' + headers.lastmod);
         }
-        return context.done(err, results.length + ' locations saved successfully with timestamp ' + headers.lastmod);
       });
+
     });
   }
 };
