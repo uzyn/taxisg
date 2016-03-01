a729fecc661ebbd116ab72f4c3142463f104171b
U-Zyn Chua <chua@uzyn.com>
2016-03-01 19:41:59 +0800

    React & ES6

--- app.js
+++ app.js
@@ -1,36 +1,73 @@
-var tables = {
-  grains: 'taxisg.grains',
-  locations: 'taxisg.locations'
-};
-
+/**
+ * AWS DynamoDB
+ */
 AWS.config.update({
   region: 'ap-southeast-1',
   accessKeyId: 'AKIAJEZJR6ZGINIW5YYQ',
   secretAccessKey: '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',
 });
+const docClient = new AWS.DynamoDB.DocumentClient();
 
-var docClient = new AWS.DynamoDB.DocumentClient();
-
-var params = {
-  TableName: tables.grains,
-  KeyConditionExpression: '#d = :d',
-  ExpressionAttributeNames: {
-    '#d': 'domain'
+const db = {
+  tables: {
+    grains: 'taxisg.grains',
+    locations: 'taxisg.locations'
   },
-  ExpressionAttributeValues: {
-    ':d': 1
-  },
-  Limit: 1,
-  ScanIndexForward: false
-};
 
-console.log(params);
+  latest() {
+    const params = {
+      TableName: this.tables.grains,
+      KeyConditionExpression: '#d = :d',
+      ExpressionAttributeNames: {
+        '#d': 'domain'
+      },
+      ExpressionAttributeValues: {
+        ':d': 1
+      },
+      Limit: 1,
+      ScanIndexForward: false,
+      ReturnConsumedCapacity: 'TOTAL'
+    };
 
-docClient.query(params, function (err, data) {
-  if (err) {
-    return console.log(err);
+    docClient.query(params, (err, data) => {
+      if (err) {
+        return console.log(err);
+      }
+      console.log(data);
+      console.log(data.Items[0].timestamp);
+      return data;
+    });
   }
-  console.log(data);
-  console.log(data.Items[0].timestamp);
-  return data;
+}
+//db.latest();
+
+/**
+ * React
+ */
+const Latest = React.createClass({
+  getInitialState() {
+    return {
+      timestamp: null,
+      count: null
+    };
+  },
+
+  componentDidMount() {
+    db.latest();
+  },
+
+  render() {
+    console.log(this.state);
+    return (
+      <div id="latest">
+        <h3>Currently {this.state.count} taxis on the road</h3>
+        <p>as at {this.state.timestamp} 30 seconds ago.</p>
+      </div>
+    );
+  }
 });
+
+ReactDOM.render(
+  <Latest />,
+  document.getElementById('container')
+);
