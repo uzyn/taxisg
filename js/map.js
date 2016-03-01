7dfb124fc57f67a2a1ec2e7bcfeed14189be2d39
U-Zyn Chua <chua@uzyn.com>
2016-03-01 23:58:16 +0800

    Loading of Google Maps

---
+++ js/map.js
@@ -0,0 +1,10 @@
+/**
+ * Google Maps
+ */
+var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -34.397, lng: 150.644},
+    zoom: 8
+  });
+}
